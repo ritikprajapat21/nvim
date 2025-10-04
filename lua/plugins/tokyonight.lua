87e07621c4bae2fa591edc7bbc83609b3f27dd69
Ritik <ritikprajapati084@gmail.com>
2025-10-04 10:15:49 +0530

    Sidebars and floats set to transparent

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,6 +2,10 @@ return {
   "folke/tokyonight.nvim",
   opts = {
     transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
     on_highlights = function(hl)
       hl.DiagnosticUnnecessary = {
         fg = "#636da6",
